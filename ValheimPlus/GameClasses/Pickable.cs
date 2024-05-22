d4319201bd8fd5f6134b03490dca00f550ec21a2
Jan Sobotta <cv.jsobotta@googlemail.com>
2024-05-22 23:42:14 +0200

    add pickable quest items

--- ValheimPlus/GameClasses/Pickable.cs
+++ ValheimPlus/GameClasses/Pickable.cs
@@ -88,6 +88,13 @@ namespace ValheimPlus.GameClasses
                 "BlackCore"
             };
 
+            var questItems = new List<string>
+            {
+                "DragonEgg",
+                "WitheredBone",
+                "GoblinTotem"
+            };
+
             _yieldModifierDict = new Dictionary<string, float>();
 
             foreach (var item in edibles)
@@ -102,6 +109,8 @@ namespace ValheimPlus.GameClasses
                 _yieldModifierDict.Add(item, Configuration.Current.Pickable.surtlingCores);
             foreach (var item in blackCores)
                 _yieldModifierDict.Add(item, Configuration.Current.Pickable.blackCores);
+            foreach (var item in questItems)
+                _yieldModifierDict.Add(item, Configuration.Current.Pickable.questItems);
         }
     }
 
